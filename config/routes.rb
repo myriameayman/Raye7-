cab6c517950b8893b2957d25c891bb932227ee22
myriameayman <myriameayman@gmail.com>
2015-03-22 23:01:44 +0200

    Changing production #15

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 RsgLinkedinGem::Application.routes.draw do
   get "welcome/index"
-  #root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   #devise_for :users
   devise_for :users, :controllers => {:registrations => "registrations"}
