ec9ba7f1501bb6e075932465c59b7cea01c2f94a
AhmedAdelIbrahim <ahmedadelibrahim1994@gmail.com>
2015-04-12 20:50:30 +0200

    modifying facebook auth#21

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,16 +1,46 @@
 class FacebookController < ApplicationController
-	before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
   def create
-  	@user = current_user
-  	@user.provider = env["omniauth.auth"].provider
+    @user = current_user
+   
+    @fb_friends = FbGraph::User.me(env["omniauth.auth"].credentials.token).friends
+    @graph = Koala::Facebook::API.new(env["omniauth.auth"].credentials.token)
+    profile = @graph.get_object("me")
+
+    friends = @graph.get_connections("me", "friends")
+    @user.name = friends.count
+    @user.provider = env["omniauth.auth"].provider
     @user.uid = env["omniauth.auth"].uid
     @user.fb_email = env["omniauth.auth"].info.email
     @user.faceboook_profileimage=env["omniauth.auth"].info.image
     @user.oauth_token = env["omniauth.auth"].credentials.token
+    
+    unless(Circle.exists?(:name => "facebook"))
+      fbCircle = Circle.new 
+      fbCircle.user_id = current_user.id 
+      fbCircle.name = "facebook"
+      fbCircle.save
+    else
+      fbCircle =  Circle.find_by name: 'facebook'
+    end
+
+    friends.each do |f| 
+        fbFriend = Friend.new 
+        fbFriend.circle_id = fbCircle.id   
+        fbFriend.name = f["name"]
+        fbFriend.fb_id = f["id"] 
+        fbFriend.save  
+    end       
+
+   
+    Koala.config.api_version = "v2.0"
+
     @user.save!
     redirect_to root_path
   end
+
+  
 
   def destroy
     session[:user_id] = nil
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '716016431848330', '2ca26a95b3eaf85141826fba125d365e', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+    provider :facebook, '716016431848330', '2ca26a95b3eaf85141826fba125d365e', {:scope => 'user_friends, email, public_profile',:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
 end
