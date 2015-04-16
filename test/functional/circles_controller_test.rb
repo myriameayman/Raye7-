c74ed6148bc3a4cf23f3abcb1aa43b0c54e21ff4
ebrahim-elgaml <ebrahim.el.gaml@gmail.com>
2015-04-16 18:07:10 +0200

    adding test for showing nil circle  #38

--- test/functional/circles_controller_test.rb
+++ test/functional/circles_controller_test.rb
@@ -52,6 +52,21 @@ class CirclesControllerTest < ActionController::TestCase
     get(:show, {'id' => "2"})
     assert_redirected_to "http://test.host/"
   end
+test "should not get show circle nil" do
+    @user = User.new(:email => 'test@example.com',:username => 'test', 
+      :password => 'password', :password_confirmation => 'password', :firstName => 'test',
+       :lastName => 'test') 
+    @circle = Circle.new 
+    @circle.name = "facebook"
+    @circle.id = 1
+    @circle.user_id = @user.id 
+    @circle.save
+    @user.circles << @circle
+    @user.save
+    sign_in @user
+    get(:show, {'id' => nil})
+    assert_redirected_to "http://test.host/"
+  end
 
 
   #test "should get show out of range" do
