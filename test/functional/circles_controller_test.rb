8ed315971b8f5fae5e02468011ca1a46ea882bd7
ebrahim-elgaml <ebrahim.el.gaml@gmail.com>
2015-04-16 18:14:23 +0200

    removing unneeded comments from circle test  #38

--- test/functional/circles_controller_test.rb
+++ test/functional/circles_controller_test.rb
@@ -2,22 +2,6 @@ require 'test_helper'
 
 class CirclesControllerTest < ActionController::TestCase
   include Devise::TestHelpers
-  #test "should get index" do
-   # @user = User.where("id = '1'")
-   # session[:user_id] = @user.id
-   # get :index
-   # assert_response :success
-  #end
-  
-  #before_filter :do_not_authenticate
-
-  #test "should get show" do
-    #@user = User.where("id = '1'")
-    #session[:user_id] = @user.id
-   # get :show
-   # assert_response :success
-  #end
-
   test "should get show circle" do
     @user = User.new(:email => 'test@example.com',:username => 'test', 
       :password => 'password', :password_confirmation => 'password', :firstName => 'test',
@@ -32,9 +16,6 @@ class CirclesControllerTest < ActionController::TestCase
     sign_in @user
     get(:show, {'id' => "1"})
     assert :sucess
-    #get(:show, {'id' => "5"})
-    #assert_not_nil( :circle, ["the circle is nil"] )
-   #assert_response :success
   end
   
   test "should not get show circle" do
@@ -52,7 +33,8 @@ class CirclesControllerTest < ActionController::TestCase
     get(:show, {'id' => "2"})
     assert_redirected_to "http://test.host/"
   end
-test "should not get show circle nil" do
+
+  test "should not get show circle nil" do
     @user = User.new(:email => 'test@example.com',:username => 'test', 
       :password => 'password', :password_confirmation => 'password', :firstName => 'test',
        :lastName => 'test') 
@@ -68,27 +50,5 @@ test "should not get show circle nil" do
     assert_redirected_to "http://test.host/"
   end
 
-
-  #test "should get show out of range" do
-    #@user = User.where("id = '1'")
-    #session[:user_id] = @user.id
-    #get(:show, {'id' => "500000"})
-   # assert_response :success
-  #end
-
-  #test "should get create" do
-   # @user = User.where("id = '1'")
-   # session[:user_id] = @user.id
-   # get :create
-  #  assert_response :success
- # end
-
-  #test "should get delete" do
-    #@user = User.where("id = '1'")
-    #session[:user_id] = @user.id
-    #get :delete
-   # assert_response :success
-  #end
-  
 
 end
