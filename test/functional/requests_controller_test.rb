5bc3673b093cd93a235e15e768c18663cc917030
renadshaaban <renad.shaaban@gmail.com>
2015-04-18 00:19:20 +0200

    test for joining trip valid and unvalid , test for show request #37

--- test/functional/requests_controller_test.rb
+++ test/functional/requests_controller_test.rb
@@ -1,11 +1,10 @@
-
 require 'test_helper'
 
 class RequestsControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
   test "should get create" do
-    @user = User.new(:email => 'test7@example.com',:username => 'renad', :password => 'password',
+    @user = User.new(:email => 'test1@example.com',:username => 'myriame', :password => 'password',
     :password_confirmation => 'password', :firstName => 'Aaaaaadsa', :lastName => 'aasdasdsad')
     @user.save
     sign_in @user
@@ -15,8 +14,12 @@ class RequestsControllerTest < ActionController::TestCase
   
   
   test "should get userProfile" do
+    @user = User.new(:email => 'test1@example.com',:username => 'myriame', :password => 'password',
+    :password_confirmation => 'password', :firstName => 'Aaaaaadsa', :lastName => 'aasdasdsad')
+    @user.save
+    sign_in @user
     get :create_ride_info
-    String url = "http://test.host/profiles/myAccount"
+    String url = "http://test.host/"
     assert_redirected_to url 
   end
 
@@ -31,7 +34,7 @@ class RequestsControllerTest < ActionController::TestCase
   test "check requests validations integers only" do
     request = Request.new :seats => "a", :car_color => "rouge", :long_destination => 5,:lat_destination => 5, 
     :long_curr => 4,:lat_curr => 4, :car_number => "28-2180", :air_conditioner => 1, :smoking => 1, :trunk => 0,
-    :user_id => 2   
+    :user_id => 1   
     assert !request.valid?
   end
   
@@ -44,17 +47,21 @@ class RequestsControllerTest < ActionController::TestCase
   
   
   test "should get home" do
-   #@user = User.new(:email => 'test@example.com',:username => 'ebrahim', :password => 'password',
-    #:password_confirmation => 'password', :firstName => 'Aaaaaadsa', :lastName => 'aasdasdsad')
-   #@user.save
-   #sign_in @user
-   #get :new 
-   #String url = "http://test.host/home"
-   #assert_redirected_to url 
+   @user = User.new(:email => 'renad@example.com',:username => 'renad', :password => 'password',
+    :password_confirmation => 'password', :firstName => 'Aaaaaadsa', :lastName => 'aasdasdsad')
+   @user.save
+   sign_in @user
+   get :new 
+   String url = "http://test.host/home"
+   assert_redirected_to url 
   end
   
   
   test "should get myProfile" do
+    @user = User.new(:email => 'test1@example.com',:username => 'myriame', :password => 'password',
+    :password_confirmation => 'password', :firstName => 'Aaaaaadsa', :lastName => 'aasdasdsad')
+    @user.save
+    sign_in @user
     @@i = nil 
     get :home 
     String url = "http://test.host/profiles/myAccount"
@@ -63,6 +70,10 @@ class RequestsControllerTest < ActionController::TestCase
   
   
   test "should get home2" do
+    @user = User.new(:email => 'test1@example.com',:username => 'myriame', :password => 'password',
+    :password_confirmation => 'password', :firstName => 'Aaaaaadsa', :lastName => 'aasdasdsad')
+    @user.save
+    sign_in @user
     @@i = 1 
     get :create_curr_location
     String url = "http://test.host/home"
@@ -71,11 +82,28 @@ class RequestsControllerTest < ActionController::TestCase
   
   
   test "should get create2" do
+    @user = User.new(:email => 'test1@example.com',:username => 'myriame', :password => 'password',
+    :password_confirmation => 'password', :firstName => 'Aaaaaadsa', :lastName => 'aasdasdsad')
+    @user.save
+    sign_in @user
     @@i = 2 
     @@request = Request.new
     get :create_curr_location
     String url = "http://test.host/requests/create"
     assert_redirected_to url 
   end
+
+  test "should show request" do
+    @user =User.new(:email =>'test2@example.com',:username => 'adel', :password => 'password',
+    :password_confirmation => 'password', :firstName => 'Aaaaaadsa', :lastName => 'aasdasdsad')
+    @user.save
+    sign_in @user
+    @myrequest=Request.new(:seats=>1)
+    @myrequest.save
+    get :show, :id => @myrequest.id
+    assert_response :success
+  end
+
+
 
 end
