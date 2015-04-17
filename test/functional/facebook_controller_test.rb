7f96e862537a1d06bfb24f6b850a3e8e8a5ba9c6
AhmedAdelIbrahim <ahmedadelibrahim1994@gmail.com>
2015-04-17 16:55:44 +0200

    adding oauth_expires_at to the user#21

--- test/functional/facebook_controller_test.rb
+++ test/functional/facebook_controller_test.rb
@@ -1,5 +1,16 @@
 require 'test_helper'
 
 class FacebookControllerTest < ActionController::TestCase
+	include Devise::TestHelpers
+
+  test "should get create a user and insert in the DB facebook attributes name , fb_email , provider ,user_id ,auth_token,facebook_profileimage" do
+    @user = User.new(:email => 'test@example.com',:username => 'testusername', :password => 'testpassword',
+    :password_confirmation => 'testpassword', :firstName => 'test', :lastName => 'test')
+    @user.save
+    sign_in @user
+    get :create
+    assert :success 
+  end
+  
 
 end
