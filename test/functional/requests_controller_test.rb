ef1a6b6d394160babd6ae6604c0e55e5f2e82077
ebrahim-elgaml <ebrahim.el.gaml@gmail.com>
2015-04-19 08:23:23 +0200

    modyfing test  requests  #55

--- test/functional/requests_controller_test.rb
+++ test/functional/requests_controller_test.rb
@@ -62,9 +62,9 @@ class RequestsControllerTest < ActionController::TestCase
     :password_confirmation => 'password', :firstName => 'Aaaaaadsa', :lastName => 'aasdasdsad')
     @user.save
     sign_in @user
-    @@i = nil 
+    @@form_step = nil 
     get :home 
-    String url = "http://test.host/profiles/myAccount"
+    String url = "http://test.host/"
     assert_redirected_to url 
   end
   
@@ -74,11 +74,10 @@ class RequestsControllerTest < ActionController::TestCase
     :password_confirmation => 'password', :firstName => 'Aaaaaadsa', :lastName => 'aasdasdsad')
     @user.save
     sign_in @user
-    @@i = 1 
+    @@form_step = 1 
     get :create_curr_location
     String url = "http://test.host/home"
     assert_redirected_to url 
-
   end
   
   
@@ -87,22 +86,22 @@ class RequestsControllerTest < ActionController::TestCase
     :password_confirmation => 'password', :firstName => 'Aaaaaadsa', :lastName => 'aasdasdsad')
     @user.save
     sign_in @user
-    @@i = 2 
+    @@form_step = 2 
     @@request = Request.new
     get :create_curr_location
     String url = "http://test.host/requests/create"
     assert_redirected_to url 
   end
-
   test "should show request" do
     @user =User.new(:email =>'test2@example.com',:username => 'adel', :password => 'password',
     :password_confirmation => 'password', :firstName => 'Aaaaaadsa', :lastName => 'aasdasdsad')
     @user.save
     sign_in @user
-    @myrequest=Request.new(:seats=>1)
+    @myrequest=Request.new(:seats=>1,:long_curr=>4,:lat_curr=>4,:long_destination=>4,:lat_destination=>5,:car_color=>"rouge",
+      :car_number=>"34",:user_id=>@user.id)
     @myrequest.save
     get :show, :id => @myrequest.id
     assert_response :success
-
   end
+
 end
