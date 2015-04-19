3a57a2ceb3ea310ea63554deb99d51ac934d5c61
AhmedAdelIbrahim <ahmedadelibrahim1994@gmail.com>
2015-04-19 02:38:28 +0200

    fixing tests for facebook controllers #21

--- test/functional/facebook_controller_test.rb
+++ test/functional/facebook_controller_test.rb
@@ -12,56 +12,66 @@ class FacebookControllerTest < ActionController::TestCase
     #assert :success 
   #end
     test "check validations facebookMail not blank" do
-    user = User.new :faceboook_profileimage => nil,:email => 'test@example.com',:username => 'testusername', :password => 'testpassword',
-    :password_confirmation => 'testpassword', :firstName => 'test', :lastName => 'test',:fb_email=> nil,:name=> 'Ahmed Adel'
+    user = User.new :faceboook_profileimage => 'http://graph.facebook.com/816246471757866/picture?type=square',:email => 'test@example.com',:username => 'testusername', :password => 'testpassword',
+    :password_confirmation => 'testpassword', :firstName => 'test', :lastName => 'test',:fb_email=> nil,:name=> 'Ahmed Adel',:oauth_token => 'CAAKLNpES24oBAC6o6tE59Q0nHW54a6uyWPxZBmvfS1uvlpZA8qv9XJeUSa5usSAjv6TRlDwHo1sp5j1xZAAh1WjFt3Vai2EQULiyMcEZBS3k1VHBmfXcVVJ6Hd44zYTkPZA0psl8spoqwKQxzEBZAbyGWxlzONLlJFJeAu0GBGrgCRkUkC8xMPmjS6ByfoZBCIptst9ZBnlfZANKQZAgEZCoBcc'
     
     assert !user.valid?
   end
 
-     test "check validations facebookProfileimage not blank" do
-    user = User.new :faceboook_profileimage => nil,:email => 'test@example.com',:username => 'testusername', :password => 'testpassword',
-    :password_confirmation => 'testpassword', :firstName => 'test', :lastName => 'test',:fb_email=> nil,:name=> 'Ahmed Adel'
+     test "check validations uid not blank" do
+    user = User.new :faceboook_profileimage => 'http://graph.facebook.com/816246471757866/picture?type=square',:email => 'test@example.com',:username => 'testusername', :password => 'testpassword',
+    :password_confirmation => 'testpassword', :firstName => 'test', :lastName => 'test',:fb_email=> 'ahmedadelana@hotmail.com',:name=> 'Ahmed Adel',:oauth_token => 'CAAKLNpES24oBAC6o6tE59Q0nHW54a6uyWPxZBmvfS1uvlpZA8qv9XJeUSa5usSAjv6TRlDwHo1sp5j1xZAAh1WjFt3Vai2EQULiyMcEZBS3k1VHBmfXcVVJ6Hd44zYTkPZA0psl8spoqwKQxzEBZAbyGWxlzONLlJFJeAu0GBGrgCRkUkC8xMPmjS6ByfoZBCIptst9ZBnlfZANKQZAgEZCoBcc',
+    :uid => nil
     
     assert !user.valid?
   end
 
-  test "check validations uid not blank" do
-    user = User.new :faceboook_profileimage => nil,:email => 'test@example.com',:username => 'testusername', :password => 'testpassword',
-    :password_confirmation => 'testpassword', :firstName => 'test', :lastName => 'test',:fb_email=> nil,:name=> 'Ahmed Adel',:uid=>nil
+  test "check validations oauth_token not blank" do
+    user = User.new :faceboook_profileimage => 'http://graph.facebook.com/816246471757866/picture?type=square',:email => 'test@example.com',:username => 'testusername', :password => 'testpassword',
+    :password_confirmation => 'testpassword', :firstName => 'test', :lastName => 'test',:fb_email=> 'ahmedadelana@hotmail.com',:name=> 'Ahmed Adel',:oauth_token => nil,
+    :uid => '816246471757866'
     
     assert !user.valid?
   end
 
- test "check validations oauth_token not blank" do
-    user = User.new :faceboook_profileimage => nil,:email => 'test@example.com',:username => 'testusername', :password => 'testpassword',
-    :password_confirmation => 'testpassword', :firstName => 'test', :lastName => 'test',:fb_email=> nil,:name=> 'Ahmed Adel',:uid=>nil,:oauth_token=>nil
+    test "check validations facebookMail is unique" do
+    @user = User.new :faceboook_profileimage => 'http://graph.facebook.com/816246471757866/picture?type=square',:email => 'test@example.com',:username => 'testusername', :password => 'testpassword',
+    :password_confirmation => 'testpassword', :firstName => 'test', :lastName => 'test',:fb_email=> 'ahmedadelana@hotmail.com',:name=> 'Ahmed Adel',:oauth_token => 'CAAKLNpES24oBAC6o6tE59Q0nHW54a6uyWPxZBmvfS1uvlpZA8qv9XJeUSa5usSAjv6TRlDwHo1sp5j1xZAAh1WjFt3Vai2EQULiyMcEZBS3k1VHBmfXcVVJ6Hd44zYTkPZA0psl8spoqwKQxzEBZAbyGWxlzONLlJFJeAu0GBGrgCRkUkC8xMPmjS6ByfoZBCIptst9ZBnlfZANKQZAgEZCoBcc',
+    :uid => '816246471757866'
+    @user.save
+    
+    user = User.new :faceboook_profileimage => 'http://graph.facebook.com/633298913480833/picture?type=square',:email => 'test1@example.com',:username => 'testusername1', :password => 'testpassword1',
+    :password_confirmation => 'testpassword1', :firstName => 'test1', :lastName => 'test1',:fb_email=> 'ahmedadelana@hotmail.com',:name=> 'Ahmed Adel',:oauth_token => 'CAAKLNpES24oBAIWw8nirEUgQ8oGbGOJAlMg5tS8CN3VcQcv4jSZCB1LE2q2q6xAhOzPI7QLXOq1vOZAtXN32WXXlbp1ZCEoZBMHNT3K47cdIU86RUJLi2pypORP5bhcwnHFHmCRyGZBPvio1AbSzlhiodjhrOZC91DAQKhZCKTlBkCV1SyjHd55gKKfnpTuuEUZD',
+    :uid => '633298913480833'
     
     assert !user.valid?
   end
 
-  test "check validations facebookMail is unique" do
-    user = User.new :faceboook_profileimage => nil,:email => 'test@example.com',:username => 'testusername', :password => 'testpassword',
-    :password_confirmation => 'testpassword', :firstName => 'test', :lastName => 'test',:fb_email=> "ahmedadelana@hotmail.com",:name=> 'Ahmed Adel'
+test "check validations uid is unique" do
+    @user = User.new :faceboook_profileimage => 'http://graph.facebook.com/816246471757866/picture?type=square',:email => 'test@example.com',:username => 'testusername', :password => 'testpassword',
+    :password_confirmation => 'testpassword', :firstName => 'test', :lastName => 'test',:fb_email=> 'ahmedadelana@hotmail.com',:name=> 'Ahmed Adel',:oauth_token => 'CAAKLNpES24oBAC6o6tE59Q0nHW54a6uyWPxZBmvfS1uvlpZA8qv9XJeUSa5usSAjv6TRlDwHo1sp5j1xZAAh1WjFt3Vai2EQULiyMcEZBS3k1VHBmfXcVVJ6Hd44zYTkPZA0psl8spoqwKQxzEBZAbyGWxlzONLlJFJeAu0GBGrgCRkUkC8xMPmjS6ByfoZBCIptst9ZBnlfZANKQZAgEZCoBcc',
+    :uid => '816246471757866'
+    @user.save
+    
+    user = User.new :faceboook_profileimage => 'http://graph.facebook.com/633298913480833/picture?type=square',:email => 'test1@example.com',:username => 'testusername1', :password => 'testpassword1',
+    :password_confirmation => 'testpassword1', :firstName => 'test1', :lastName => 'test1',:fb_email=> 'myriameayman@yahoo.com',:name=> 'Ahmed Adel',:oauth_token => 'CAAKLNpES24oBAIWw8nirEUgQ8oGbGOJAlMg5tS8CN3VcQcv4jSZCB1LE2q2q6xAhOzPI7QLXOq1vOZAtXN32WXXlbp1ZCEoZBMHNT3K47cdIU86RUJLi2pypORP5bhcwnHFHmCRyGZBPvio1AbSzlhiodjhrOZC91DAQKhZCKTlBkCV1SyjHd55gKKfnpTuuEUZD',
+    :uid => '816246471757866'
     
     assert !user.valid?
   end
 
-  test "check validations facebookOauth-token is unique" do
-    user = User.new :faceboook_profileimage => nil,:email => 'test@example.com',:username => 'testusername', :password => 'testpassword',
-    :password_confirmation => 'testpassword', :firstName => 'test', :lastName => 'test',:fb_email=> "ahmedadelana@hotmail.com",:name=> 'Ahmed Adel',:oauth_token => 'CAAKLNpES24oBAC6o6tE59Q0nHW54a6uyWPxZBmvfS1uvlpZA8qv9XJeUSa5usSAjv6TRlDwHo1sp5j1xZAAh1WjFt3Vai2EQULiyMcEZBS3k1VHBmfXcVVJ6Hd44
-    zYTkPZA0psl8spoqwKQxzEBZAbyGWxlzONLlJFJeAu0GBGrgCRkUkC8xMPmjS6ByfoZBCIptst9ZBnlfZANKQZAgEZCoBcc'
+test "check validations oauth_token is unique" do
+    @user = User.new :faceboook_profileimage => 'http://graph.facebook.com/816246471757866/picture?type=square',:email => 'test@example.com',:username => 'testusername', :password => 'testpassword',
+    :password_confirmation => 'testpassword', :firstName => 'test', :lastName => 'test',:fb_email=> 'ahmedadelana@hotmail.com',:name=> 'Ahmed Adel',:oauth_token => 'CAAKLNpES24oBAC6o6tE59Q0nHW54a6uyWPxZBmvfS1uvlpZA8qv9XJeUSa5usSAjv6TRlDwHo1sp5j1xZAAh1WjFt3Vai2EQULiyMcEZBS3k1VHBmfXcVVJ6Hd44zYTkPZA0psl8spoqwKQxzEBZAbyGWxlzONLlJFJeAu0GBGrgCRkUkC8xMPmjS6ByfoZBCIptst9ZBnlfZANKQZAgEZCoBcc',
+    :uid => '816246471757866'
+    @user.save
+    
+    user = User.new :faceboook_profileimage => 'http://graph.facebook.com/633298913480833/picture?type=square',:email => 'test1@example.com',:username => 'testusername1', :password => 'testpassword1',
+    :password_confirmation => 'testpassword1', :firstName => 'test1', :lastName => 'test1',:fb_email=> 'myriameayman@yahoo.com',:name=> 'Ahmed Adel',:oauth_token => 'CAAKLNpES24oBAC6o6tE59Q0nHW54a6uyWPxZBmvfS1uvlpZA8qv9XJeUSa5usSAjv6TRlDwHo1sp5j1xZAAh1WjFt3Vai2EQULiyMcEZBS3k1VHBmfXcVVJ6Hd44zYTkPZA0psl8spoqwKQxzEBZAbyGWxlzONLlJFJeAu0GBGrgCRkUkC8xMPmjS6ByfoZBCIptst9ZBnlfZANKQZAgEZCoBcc',
+    :uid => '633298913480833'
     
     assert !user.valid?
   end
-
-  test "check validations facebook-uid is unique" do
-    user = User.new :faceboook_profileimage => nil,:email => 'test@example.com',:username => 'testusername', :password => 'testpassword',
-    :password_confirmation => 'testpassword', :firstName => 'test', :lastName => 'test',:fb_email=> "ahmedadelana@hotmail.com",:name=> 'Ahmed Adel',
-    :oauth_token => nil , :uid => '816246471757866'
-    
-    assert !user.valid?
-  end
-
 
 
 
