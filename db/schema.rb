4f63d1e2a28380b2a5a869a553eb387dad1856f5
ebrahim-elgaml <ebrahim.el.gaml@gmail.com>
2015-05-01 01:09:05 +0300

    setting verfication default to false   #84

--- db/schema.rb
+++ db/schema.rb
@@ -169,8 +169,8 @@ ActiveRecord::Schema.define(:version => 20150430220118) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -181,8 +181,8 @@ ActiveRecord::Schema.define(:version => 20150430220118) do
     t.string   "last_sign_in_ip"
     t.string   "firstName"
     t.string   "lastName"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.string   "username"
     t.string   "name"
     t.string   "screen_name"
@@ -199,7 +199,7 @@ ActiveRecord::Schema.define(:version => 20150430220118) do
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
     t.string   "faceboook_profileimage"
-    t.boolean  "verification"
+    t.boolean  "verification",           :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
