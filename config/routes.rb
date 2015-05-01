651ca0811c89692a4c1bce4f1701453bb4fa785f
AhmedAdelIbrahim <ahmedadelibrahim1994@gmail.com>
2015-05-01 17:18:58 +0300

    adding orderItem controller #73

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 
 RsgLinkedinGem::Application.routes.draw do 
  
+  get "order_items/create"
+
+  get "order_items/update"
+
+  get "order_items/destroy"
+
   get "carts/show"
 
   get "products/index"
