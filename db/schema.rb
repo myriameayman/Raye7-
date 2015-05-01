9bab0a8e37d20cbd95124bc9b9cba8b317fd955e
AhmedAdelIbrahim <ahmedadelibrahim1994@gmail.com>
2015-05-01 16:37:15 +0300

    adding orderItem model#73

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150430220118) do
+ActiveRecord::Schema.define(:version => 20150501133604) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trackable_id"
@@ -106,6 +106,37 @@ ActiveRecord::Schema.define(:version => 20150430220118) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "order_items", :force => true do |t|
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.decimal  "unit_price",  :precision => 12, :scale => 3
+    t.integer  "quantity"
+    t.decimal  "total_price", :precision => 12, :scale => 3
+    t.datetime "created_at",                                 :null => false
+    t.datetime "updated_at",                                 :null => false
+  end
+
+  add_index "order_items", ["order_id"], :name => "index_order_items_on_order_id"
+  add_index "order_items", ["product_id"], :name => "index_order_items_on_product_id"
+
+  create_table "order_statuses", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "orders", :force => true do |t|
+    t.decimal  "subtotal",        :precision => 12, :scale => 3
+    t.decimal  "tax",             :precision => 12, :scale => 3
+    t.decimal  "shipping",        :precision => 12, :scale => 3
+    t.decimal  "total",           :precision => 12, :scale => 3
+    t.integer  "order_status_id"
+    t.datetime "created_at",                                     :null => false
+    t.datetime "updated_at",                                     :null => false
+  end
+
+  add_index "orders", ["order_status_id"], :name => "index_orders_on_order_status_id"
+
   create_table "places", :force => true do |t|
     t.decimal  "long"
     t.decimal  "lat"
@@ -124,6 +155,14 @@ ActiveRecord::Schema.define(:version => 20150430220118) do
     t.integer  "full_profile_id"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+  end
+
+  create_table "products", :force => true do |t|
+    t.string   "name"
+    t.decimal  "price",      :precision => 12, :scale => 3
+    t.boolean  "active"
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
   end
 
   create_table "ratings", :force => true do |t|
