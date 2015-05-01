a114a6a1c45ad1d2efc3a99dfd1cb75855c42b08
myriameayman <myriameayman@gmail.com>
2015-05-01 16:54:58 +0300

    modification for code review #61

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ActiveRecord::Base
-  	has_many :visits
-  	has_many :users, through: :visits
-  	has_many :checkpoints
+    has_many :visits
+    has_many :users, through: :visits
+    has_many :checkpoints
     has_many :requests, through: :checkpoints
-  	attr_accessible :lat, :long, :name
+    attr_accessible :lat, :long, :name
 end
