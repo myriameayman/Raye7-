edae7af45dae6174d3ccee7ca9ac1f08dd659af9
ebrahim-elgaml <ebrahim.el.gaml@gmail.com>
2015-05-02 08:09:06 +0300

    documentation for myMailer #69

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -1,14 +1,17 @@
 class MyMailer < ActionMailer::Base
   # The source of the email @ebrahim-elgaml.
   default from: "ebrahim.el.gaml@gmail.com"
-  # Action to send the email .
+  
+  # Action to send verification  email on sign up .
   # @myUser is the current user to send the verification email .
   def gmail_email(user)
   	@myUser = user
     mail(from:'ebrahim.el.gaml@gmail.com',to: @myUser.email, subject: 'Confirmation Email')
   end
 
-
+  # Action to send verification  email on adding email.
+  # @myUser is the current user.
+  # @grant is the destination email.
   def add_email(user,mail)
     @myUser = user
     @grant = mail
