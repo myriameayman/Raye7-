68b89b124fc9b9d372d30e8d0faf2044ad557d79
AhmedAdelIbrahim <ahmedadelibrahim1994@gmail.com>
2015-05-02 23:46:22 +0300

    modifying documentation to controller#63

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -4,7 +4,7 @@ class FacebookController < ApplicationController
 # Checks that there is a currently authunticated user. 
   before_filter :authenticate_user!
 
-
+# BY : AhmedAdelIbrahim
 # Add Facebook attributes to a user who is signing up to RAYE7 after redirecting him to facebook auth page.
 # The variables : @user => current user , @fb_friends => get tokens of user facebook friends.  
                 # @graph => token of the user profile , profile => current user profile.
