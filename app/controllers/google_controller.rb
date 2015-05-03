2add1577a27ddb7dfeb32697c60efd2a2666203d
AhmedAdelIbrahim <ahmedadelibrahim1994@gmail.com>
2015-05-03 04:16:36 +0300

    modifying google controller spaces and tabs#63

--- app/controllers/google_controller.rb
+++ app/controllers/google_controller.rb
@@ -3,25 +3,22 @@ class GoogleController < ApplicationController
 # Checks that there is a currently authunticated user. 
 before_filter :authenticate_user!
 
-# BY : AhmedAdelIbrahim
+# BY : AhmedAdelIbrahim.
 # Add Google plus attributes to a user who want to add more circles to his RAYE7 account.
 # The variables : @user => current user , @provide1 => get the name of the provider.  
                 # @user.gmail => user's gmail.
-    def create
-  	@user = current_user
-  	@user.provide1 = env["omniauth.auth"].provider
-    
-  	@user.gmail=env["omniauth.auth"].info.email
-    
+  def create
+    @user = current_user
+    @user.provide1 = env["omniauth.auth"].provider
+    @user.gmail=env["omniauth.auth"].info.email
     @user.save! 
     redirect_to root_path
-  
-    end
+  end
     
-# BY : AhmedAdelIbrahim
+# BY : AhmedAdelIbrahim.
 # Destroy a session opened.
   def destroy
-  	session[:user_id] = nil
+    session[:user_id] = nil
     redirect_to root_path
   end
 end
