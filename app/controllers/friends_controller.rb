d901e1c6d717a2ae0e02d1e4956b76f83eaa222a
som3a123 <ahmad.a.elfiky@gmail.com>
2015-05-06 00:27:51 +0300

    adding documentation #88

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,9 +1,12 @@
 class FriendsController < ApplicationController
+
+  # index uses the method search that searches for a specific friend
   def index
     unless(params[:search==nil])
       @friend = Friend.search(params[:search])
     end
   end
+  # show also gets a specific friend
   def show
   	unless(params[:search==nil])
       @friend = Friend.search(params[:search])
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -11,46 +11,45 @@ class TripsController < ApplicationController
 # TODO : printing confirmation msg after joining a trip.
 # TODO : printing error msg if the user cannot be added to a ride.
 # The vaiables :@id is the user id.
-#			   :@@trip.user_id assigns user id to trip.user_id.
+#              :@@trip.user_id assigns user id to trip.user_id.
 #              :@@trip.request_id assigns request id to trip.request_id.
 def create
-	unless(params[:id==nil])
-		  @id = current_user.id
-		  @@trip.user_id = @id 
-		  @@trip.request_id= params[:id]
-		  unless(params[:id] == nil)
-		    @request=Request.find(params[:id])
-		    @owner = User.find(@request.user_id)
-		    @user = current_user
-		    @points = calc(@request.distance)
-		    @owner.budget = @owner.budget + @points
-		    @user.budget = @user.budget - @points
-
-		  	if(@request.seats>0 and @user.budget>=@points)
-		  	 @request.seats=@request.seats - 1
-		  	 @request.save
-		  	 @owner.save
-		  	 @user.save
-		 	 @@trip.save
-			end
-		  end
-		  redirect_to "/"
-	end
+    unless(params[:id==nil])
+          @id = current_user.id
+          @@trip.user_id = @id 
+          @@trip.request_id= params[:id]
+          unless(params[:id] == nil)
+            @request=Request.find(params[:id])
+            @owner = User.find(@request.user_id)
+            @user = current_user
+            @points = getpoints(@request.distance)
+            @owner.budget = @owner.budget + @points
+            @user.budget = @user.budget - @points
+            if(@request.seats>0 and @user.budget>=@points)
+              @request.seats=@request.seats - 1
+              @request.save
+              @owner.save
+              @user.save
+              @@trip.save
+            end
+          end
+          redirect_to "/"
+    end
 end
 
-# method calc to calculate points according to the distance estimated for the trip
+# method getpoints to calculate points (budget) according to the distance estimated for the trip
 # added by: Ahmed Amr Ismail
-def calc(distance)
-	liters = distance/10
-	liter_price = liters * 3
-	liter_price.ceil / 5
+def getpoints(distance)
+    liters = distance/10
+    liter_price = liters * 3
+    liter_price.ceil / 5
 end
 
 
 # The def index finds all trips of a specific request.
 # The variables :@trips :@trips is list of all available trips of a request.
 def index 
-	unless(params[:id==nil])
+    unless(params[:id==nil])
      @trips = Trip.find(params[:id])
     end
 end
