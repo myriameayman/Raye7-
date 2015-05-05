65280ab1d132257dbb2740824b5c3a1366f0cd92
ebrahim-elgaml <ebrahim.el.gaml@gmail.com>
2015-05-05 17:58:03 +0300

    add documentation send-feedback#create action  #74

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -5,14 +5,15 @@ class FeedbackController < ApplicationController
 
   end
   
+
+  # Creating the form and send it to the user and webmaster @ebrahimelgaml
   def create
     @user = current_user
     @title = params[:title]
     @message = params[:message]
     MyMailer.send_feedback(@user,@title,@message).deliver
     MyMaile.send_feedback_to_user(@user,@title,@message).deliver
-    redirect_to root_path
-  
+    redirect_to root_path  
   end
 
 end
