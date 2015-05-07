5671da215e0afd5cc4d90d3a3b2cb4f080d4e162
ebrahim-elgaml <ebrahim.el.gaml@gmail.com>
2015-05-07 06:48:54 +0300

    solving  condlicts #74

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,12 +2,6 @@ class Friend < ActiveRecord::Base
   	attr_accessible :circle_id, :name
   	validates :app_id, presence: true
   	belongs_to :circle
-<<<<<<< HEAD
-  	validates :app_id, presence: true
-
-=======
->>>>>>> 50d541599773cdea3b3515bdeee36a131b5cbbc3
-
   	def self.search(search)
 	  if search
 	    find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
@@ -15,10 +9,5 @@ class Friend < ActiveRecord::Base
 	    find(:all)
 	  end
 	end
-<<<<<<< HEAD
 
-  	
-
-=======
->>>>>>> 50d541599773cdea3b3515bdeee36a131b5cbbc3
 end
